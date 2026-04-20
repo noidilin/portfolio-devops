b92fe2d7aefed9830823318961de5048da3b09fc
noidilin <linganinja.0120@gmail.com>
2026-04-20 13:31:57 +0800

    fix(lab): required variables need to be explicitly passed in
    .auto.tfvars can only be passed as the root module's variable, and it
    won't be passed to child/reusable module automatically

--- labs/01-single-web-server/stage/data-stores/mysql/main.tf
+++ labs/01-single-web-server/stage/data-stores/mysql/main.tf
@@ -1,3 +1,6 @@
 module "mysql" {
   source = "../../../modules/data-stores/mysql"
+
+  db_username = var.db_username
+  db_password = var.db_password
 }
--- labs/01-single-web-server/stage/services/webserver-cluster/main.tf
+++ labs/01-single-web-server/stage/services/webserver-cluster/main.tf
@@ -1,3 +1,9 @@
 module "webserver_cluster" {
   source = "../../../modules/services/webserver-cluster"
+
+  min_size               = 2
+  max_size               = 4
+  cluster_name           = "webservers-stage"
+  db_remote_state_bucket = "noidilin-tf-state"
+  db_remote_state_key    = "labs/01-single-web-server/stage/data-stores/mysql/terraform.tfstate"
 }
