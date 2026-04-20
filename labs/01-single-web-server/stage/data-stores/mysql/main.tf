642cde09bd6e0a1424553433520302c99eb813ba
noidilin <linganinja.0120@gmail.com>
2026-04-20 12:57:16 +0800

    feat(lab): migrate with the shared modules for existing components

--- labs/01-single-web-server/stage/data-stores/mysql/main.tf
+++ labs/01-single-web-server/stage/data-stores/mysql/main.tf
@@ -1,11 +1,3 @@
-resource "aws_db_instance" "mysql" {
-  identifier_prefix   = "single-web-server"
-  engine              = "mysql"
-  allocated_storage   = 10
-  instance_class      = "db.t3.micro"
-  skip_final_snapshot = true
-  db_name             = "single_web_server_db"
-
-  username = var.db_username
-  password = var.db_password
+module "mysql" {
+  source = "../../../modules/data-stores/mysql"
 }
--- labs/01-single-web-server/stage/services/webserver-cluster/outputs.tf
+++ labs/01-single-web-server/stage/services/webserver-cluster/outputs.tf
@@ -1,9 +1,9 @@
 output "alb_dns_name" {
   description = "The domain name of the load balancer."
-  value       = aws_lb.web_alb.dns_name
+  value       = module.webserver_cluster.alb_dns_name
 }
 
 output "asg_name" {
   description = "The name of the Auto Scaling Group"
-  value       = aws_autoscaling_group.web_asg.name
+  value       = module.webserver_cluster.asg_name
 }
