9a9eb4a6e9d1648934ea96037cdf0b84d4e7cda6
noidilin <linganinja.0120@gmail.com>
2026-04-20 14:47:23 +0800

    feat(lab): add auto scaling schedule for prod env

--- labs/01-single-web-server/prod/services/webserver-cluster/main.tf
+++ labs/01-single-web-server/prod/services/webserver-cluster/main.tf
@@ -7,3 +7,23 @@ module "webserver_cluster" {
   db_remote_state_bucket = "noidilin-tf-state"
   db_remote_state_key    = "labs/01-single-web-server/prod/data-stores/mysql/terraform.tfstate"
 }
+
+# This feature is production only feature
+# load exported values from module
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  scheduled_action_name  = "scale-out-during-business-hours"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 10
+  recurrence             = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  scheduled_action_name  = "scale-in-at-night"
+  min_size               = 2
+  max_size               = 10
+  desired_capacity       = 2
+  recurrence             = "0 17 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
