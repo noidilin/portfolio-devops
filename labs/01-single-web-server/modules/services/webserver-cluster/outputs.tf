598023241fb389d5fe98aa41567a0b5eb63b5c99
noidilin <linganinja.0120@gmail.com>
2026-04-21 12:56:39 +0800

    feat(lab): append ingress rule for stage env based on module output SG id

--- labs/01-single-web-server/modules/services/webserver-cluster/outputs.tf
+++ labs/01-single-web-server/modules/services/webserver-cluster/outputs.tf
@@ -7,3 +7,8 @@ output "asg_name" {
   description = "The name of the Auto Scaling Group"
   value       = aws_autoscaling_group.web_asg.name
 }
+
+output "alb_security_group_id" {
+  description = "The ID of the Security Group attached to the load balancer"
+  value       = aws_security_group.web_alb_sg.id
+}
--- labs/01-single-web-server/stage/services/webserver-cluster/main.tf
+++ labs/01-single-web-server/stage/services/webserver-cluster/main.tf
@@ -7,3 +7,12 @@ module "webserver_cluster" {
   db_remote_state_bucket = "noidilin-tf-state"
   db_remote_state_key    = "labs/01-single-web-server/stage/data-stores/mysql/terraform.tfstate"
 }
+
+resource "aws_vpc_security_group_ingress_rule" "web_alg_allow_testing_inbound" {
+  security_group_id = module.webserver_cluster.alb_security_group_id
+
+  from_port   = 12345
+  to_port     = 12345
+  ip_protocol = "tcp"
+  cidr_ipv4   = "0.0.0.0/0"
+}
