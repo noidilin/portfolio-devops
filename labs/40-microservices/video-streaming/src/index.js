5aac3f5f86bcf41c337ec7edfba7b68e7bdc758b
noidilin <linganinja.0120@gmail.com>
2026-05-15 16:12:06 +0800

    feat(app): add azure storage to access assets and streaming video with hard coded path

--- labs/40-microservices/video-streaming/src/index.js
+++ labs/40-microservices/video-streaming/src/index.js
@@ -1,37 +1,56 @@
 const express = require("express");
-const fs = require("fs");
+const http = require("http");
 
 //
-// Throws an error if the PORT environment variable is missing.
+// Throws an error if the any required environment variables are missing.
 //
+
 if (!process.env.PORT) {
     throw new Error("Please specify the port number for the HTTP server with the environment variable PORT.");
 }
 
+if (!process.env.VIDEO_STORAGE_HOST) {
+    throw new Error("Please specify the host name for the video storage microservice in variable VIDEO_STORAGE_HOST.");
+}
+
+if (!process.env.VIDEO_STORAGE_PORT) {
+    throw new Error("Please specify the port number for the video storage microservice in variable VIDEO_STORAGE_PORT.");
+}
+
 //
-// Extracts the PORT environment variable.
+// Extracts environment variables to globals for convenience.
 //
 const PORT = process.env.PORT;
+const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+console.log(`Forwarding video requests to ${VIDEO_STORAGE_HOST}:${VIDEO_STORAGE_PORT}.`);
 
 const app = express();
 
 //
 // Registers a HTTP GET route for video streaming.
 //
-app.get("/video", async (req, res) => {
-    const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
-    const stats = await fs.promises.stat(videoPath);
-
-    res.writeHead(200, {
-        "Content-Length": stats.size,
-        "Content-Type": "video/mp4",
-    });
-    fs.createReadStream(videoPath).pipe(res);
+app.get("/video", (req, res) => {
+    const forwardRequest = http.request( // Forward the request to the video storage microservice.
+        {
+            host: VIDEO_STORAGE_HOST,
+            port: VIDEO_STORAGE_PORT,
+            path: '/video?path=SampleVideo_1280x720_1mb.mp4', // Video path is hard-coded for the moment.
+            method: 'GET',
+            headers: req.headers
+        }, 
+        forwardResponse => {
+            res.writeHeader(forwardResponse.statusCode, forwardResponse.headers);
+            forwardResponse.pipe(res);
+        }
+    );
+    
+    req.pipe(forwardRequest);
 });
 
 //
 // Starts the HTTP server.
 //
 app.listen(PORT, () => {
-    console.log(`Microservice listening on port ${PORT}, point your browser at http://localhost:${PORT}/video`);
+    console.log(`Microservice online`);
 });
